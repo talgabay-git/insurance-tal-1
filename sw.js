9183d20d01c075fcd9a12b68fcf20085ca0ba8fc
Tal Gabay <office@pf-invest.co.il>
2026-08-14 21:12:24 +0300

    תיקון מרווח סימטרי מעל ומתחת לבאנר ההתקנה בפוטר
    הוגדל margin-top לאותו ערך של margin-bottom (2rem לכל צד) כדי
    שהמרחק מעמודות הפוטר שמעל יהיה זהה למרחק מהשורה שמתחת. גרסת מטמון ל-v9.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    אסטרטגיה: דפי HTML - network-first (תוכן עדכני, נופל לגרסה שמורה כשאין רשת).
    נכסים סטטיים (CSS/JS/תמונות) - cache-first (מהירות, נופל לרשת אם חסר). */
 
-const CACHE_VERSION = 'talgabay-v8';
+const CACHE_VERSION = 'talgabay-v9';
 const APP_SHELL = [
   '/',
   '/style.css',
